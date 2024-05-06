d2ea4335a9b14037d8cf18306e24682c1b69d22c
Lokkaze <74102422+Lokkaze@users.noreply.github.com>
2024-05-06 20:02:06 +0200

    Exam question edit

--- src/views/examQuestion/index.vue
+++ src/views/examQuestion/index.vue
@@ -1,18 +1,61 @@
 <template>
   <div v-loading="loading">
-    <el-form ref="dataform" label-position="left" label-width="70px">
-        <el-row v-for="question in questions" :key="question.questionId" style="margin-bottom:50px;margin-top:10px;">
+    <el-form v-if="edit" ref="dataform" label-position="left" label-width="70px">
+        <el-row v-for="question in temp" :key="question.questionId" style="margin-bottom:20px;margin-top:10px;">
+            <el-col class="text-center">
+                <el-card class="box-card">
+                    <div slot="header" class="clearfix">
+                        <el-button style="color:#ff4949" @click="deleteQuestion(question.questionId-1)">
+                            Delete question
+                        </el-button>
+                        <span>
+                            {{ question.questionId }}. 
+                            <el-select v-model="question.type" clearable>
+                                <el-option v-for="item in typeOptions" :key="item" :label="item" :value="item"></el-option>
+                            </el-select>
+                            <el-input v-model="question.title"></el-input>
+                        </span>
+                    </div>
+                    <div v-if="question.type === 'SingleChoice' || question.type === 'MultipleChoice'">
+                        <div v-for="answer in question.examQuestionAnswers" :key="answer.examQuestionAnswersId" style="margin-bottom:10px;">
+                            <el-button @click="deleteAnswer(question.questionId-1, answer.answerId-1)">
+                                <i style="color:#ff4949" class="el-icon-delete" />
+                            </el-button>
+                            <el-input v-model="answer.content"></el-input>
+                        </div>
+                        <el-button type="primary" @click="addAnswer(question.questionId)">
+                            Add
+                        </el-button>
+                    </div>
+                    <div v-if="question.type === 'Quiz'">
+                        <el-input class="edit-input" placeholder="Enter your answer here"></el-input>
+                    </div>
+                </el-card>
+            </el-col>
+        </el-row>
+        <el-button type="primary" @click="addQuestion">
+            Add new question
+        </el-button>
+        <el-button type="primary" @click="confirmEdit">
+            Confirm edit
+        </el-button>
+        <el-button type="primary" @click="cancelEdit">
+            Cancel edit
+        </el-button>
+    </el-form>
+    <el-form v-else ref="dataform" label-position="left" label-width="70px">
+        <el-row v-for="question in questions" :key="question.questionId" style="margin-bottom:20px;margin-top:10px;">
             <el-col class="text-center">
                 <el-card class="box-card">
                     <div slot="header" class="clearfix">
                         <span>{{ question.questionId }}. {{ question.title }}</span>
                     </div>
                     <div v-if="question.type === 'SingleChoice'">
-                        <el-radio-group>
+                        <el-radio-group v-model="answerList[question.questionId-1].answer">
                             <div v-for="answer in question.examQuestionAnswers" :key="answer.examQuestionAnswersId" style="margin-bottom:5px;">
                                 <el-radio :label="answer.answerId" border>
                                 {{ answer.content }}
-                            </el-radio>
+                                </el-radio>
                             </div>
                         </el-radio-group>
                     </div>
@@ -29,25 +72,33 @@
                         </el-checkbox>
                     </div>
                     <div v-if="question.type === 'Quiz'">
-                        <el-input class="edit-input" placeholder="Enter your answer here"></el-input>
+                        <el-input class="edit-input" placeholder="Enter your answer here" v-model="answerList[question.questionId-1].answer"></el-input>
                     </div>
                 </el-card>
             </el-col>
         </el-row>
+        <el-button type="primary" style="margin-right:10px;" @click="startEdit">
+            Edit
+        </el-button>
     </el-form>
 
   </div>
 </template>
 
 <script>
-import { fetchQuestions } from '../../api/exam'
+import { title } from '@/settings'
+import { fetchQuestions, updateQuestions } from '../../api/exam'
 export default {
     data(){
         return{
             examId: 0,
             loading: true,
             questions: null,
-            answerList: []
+            answerList: [],
+            temp: null,
+            edit: false,
+            tempAnswerList: [],
+            typeOptions: ['SingleChoice', 'MultipleChoice', 'Quiz']
         }
     },
     created(){
@@ -59,6 +110,7 @@ export default {
             this.loading = true
             fetchQuestions(examId).then(response => {
                 this.questions = response.data.questions
+                this.answerList = []
                 for(let question of this.questions){
                     let answer
                     switch(question.type){
@@ -86,7 +138,6 @@ export default {
                             break
                     }
                 }
-                console.log(this.answerList)
                 this.loading = false
             }).catch(err => {
                 console.log(err)
@@ -102,6 +153,64 @@ export default {
                 this.answerList[id].selectAll = false
 
             }
+        },
+        startEdit(){
+            this.temp = JSON.parse(JSON.stringify(this.questions))
+            this.edit = true
+        },
+        deleteQuestion(questionIndex){
+            this.temp.splice(questionIndex, 1)
+            for(let i = questionIndex; i < this.temp.length; i++) {
+                this.temp[i].questionId--
+            }
+        },
+        addQuestion(){
+            let newQuestionId = 1
+            if(this.temp[this.temp.length-1].questionId){
+                newQuestionId = this.temp[this.temp.length-1].questionId+1
+            }
+            let newQuestion = {
+                examId: this.examId,
+                questionId: newQuestionId,
+                type: 'SingleChoice',
+                title: '',
+                examQuestionAnswers: []
+            }
+            this.temp.push(newQuestion)
+        },
+        deleteAnswer(questionIndex, answerIndex){
+            this.temp[questionIndex].examQuestionAnswers.splice(answerIndex, 1)
+            for(let i = answerIndex; i < this.temp[questionIndex].examQuestionAnswers.length; i++) {
+                this.temp[questionIndex].examQuestionAnswers[i].answerId--
+            }
+        },
+        addAnswer(questionId){
+            let newAnswerId = 1
+            if(this.temp[questionId-1].examQuestionAnswers[this.temp[questionId-1].examQuestionAnswers.length-1]){
+                newAnswerId = this.temp[questionId-1].examQuestionAnswers[this.temp[questionId-1].examQuestionAnswers.length-1].answerId+1
+            }
+            let newAnswer = {
+                examId: this.examId,
+                questionId: questionId,
+                content: '',
+                answerId: newAnswerId
+            }
+            this.temp[questionId-1].examQuestionAnswers.push(newAnswer)
+        },
+        confirmEdit(){
+            updateQuestions(this.examId, this.temp).then(() => {
+                this.fetchData(this.examId)
+                this.edit = false
+                this.$notify({
+                    title: 'Success',
+                    message: 'Created Successfully',
+                    type: 'success',
+                    duration: 2000
+                })
+            })
+        },
+        cancelEdit(){
+            this.edit = false
         }
     }
 }
